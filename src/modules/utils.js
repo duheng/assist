8cdd3319a55b28d8b9d0b4c9cd61e23b883c9db9
Mozi <qitmac@MozideMacBook-Pro.local>
2021-05-13 20:16:00 +0800

    updaate

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -88,9 +88,39 @@ const parseTagText = (target) => {
     }
 }
 
+const ajax = {
+    get: (url, fn) => {
+      // XMLHttpRequest对象用于在后台与服务器交换数据   
+      var xhr = new XMLHttpRequest();            
+      xhr.open('GET', url, true);
+      xhr.onreadystatechange = () => {
+        // readyState == 4说明请求已完成
+        if (xhr.readyState == 4 && xhr.status == 200 || xhr.status == 304) { 
+          // 从服务器获得数据 
+          fn.call(this, xhr.responseText);  
+        }
+      };
+      xhr.send();
+    },
+    // datat应为'a=a1&b=b1'这种字符串格式，在jq里如果data为对象会自动将对象转成这种字符串格式
+    post:  (url, data, fn) => {
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", url, true);
+      // 添加http头，发送信息至服务器时内容编码类型
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");  
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 304)) {
+          fn.call(this, xhr.responseText);
+        }
+      };
+      xhr.send(data);
+    }
+  }
+
 export {
     cookie,
     addEvent,
     removeEvent,
-    parseTagText
+    parseTagText,
+    ajax
 };
